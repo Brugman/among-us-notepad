3a0e8ad53bf6deb2c5a673265a5890239cf835a0
Tim Brugman <tim@timbrugman.com>
2021-06-16 04:41:33 +0200

    Merge PHP includes

--- public_html/includes/core-foot.php
+++
@@ -1,7 +0,0 @@
-<!-- jquery -->
-<script src="./assets/vendor/jquery-3.6.0.min.js"></script>
-<!-- app js -->
-<script src="./assets/aun.js?v202106160428"></script>
-
-</body>
-</html>
--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <!-- meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- title -->
+    <title>Among Us Notepad</title>
+
+    <!-- app css -->
+    <link rel="stylesheet" href="./assets/aun.css?v202106160428">
+
+</head>
+<body>
+
 <?php
 
-include 'includes/core-head.php';
+$players = [
+    [ 'name' => 'red', 'color' => '#C61111' ],
+    [ 'name' => 'blue', 'color' => '#132ED2' ],
+    [ 'name' => 'green', 'color' => '#11802D' ],
+    [ 'name' => 'pink', 'color' => '#EE54BB' ],
+    [ 'name' => 'orange', 'color' => '#F07D0D' ],
+    [ 'name' => 'yellow', 'color' => '#F6F657' ],
+    [ 'name' => 'black', 'color' => '#3F474E' ],
+    [ 'name' => 'white', 'color' => '#D7E1F1' ],
+    [ 'name' => 'purple', 'color' => '#6B2FBC' ],
+    [ 'name' => 'brown', 'color' => '#71491E' ],
+    [ 'name' => 'cyan', 'color' => '#38FFDD' ],
+    [ 'name' => 'lime', 'color' => '#50F039' ],
+    [ 'name' => 'maroon', 'color' => '#5F1D2E' ],
+    [ 'name' => 'rose', 'color' => '#ECC0D3' ],
+    [ 'name' => 'banana', 'color' => '#F0E7A8' ],
+    [ 'name' => 'gray', 'color' => '#758593' ],
+    [ 'name' => 'tan', 'color' => '#918877' ],
+    [ 'name' => 'coral', 'color' => '#D76464' ],
+];
 
-include 'includes/app.php';
+?>
+<table class="active-players">
+<?php
 
-include 'includes/core-foot.php';
+foreach ( $players as $id => $player )
+{
+?>
+    <tr class="player" data-status="neutral" data-id="<?=str_pad( ++$id, 3, '0', STR_PAD_LEFT );?>">
+        <td class="color"><div class="inner js-toggle-unused" title="toggle use / dont use" style="background-color: <?=$player['color'];?>;"></div></td>
+        <td class="name"><input type="text" value="<?=strtoupper( $player['name'] );?>"></td>
+        <td class="label js-toggle" data-set-status="imposter">imposter</td>
+        <td class="label js-toggle" data-set-status="sus">sus</td>
+        <td class="label js-toggle" data-set-status="neutral">neutral</td>
+        <td class="label js-toggle" data-set-status="trusty">trusty</td>
+        <td class="label js-toggle" data-set-status="crewmate">crewmate</td>
+        <td class="label js-toggle" data-set-dead="dead">dead</td>
+    </tr>
+<?php
+}
 
+?>
+</table>
+
+<p class="resets"><a href="/">new lobby</a><a class="js-reset">new game</a></p>
+
+<table class="unused-players"></table>
+
+<!-- jquery -->
+<script src="./assets/vendor/jquery-3.6.0.min.js"></script>
+<!-- app js -->
+<script src="./assets/aun.js?v202106160428"></script>
+
+</body>
+</html>
